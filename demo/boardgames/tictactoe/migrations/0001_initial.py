449fd837cf804bf950910f327a8385134ac9b32f
nashfleet <nashf170@gmail.com>
2017-03-29 22:59:00 -0500

    update migrations and rest of font files

--- demo/boardgames/tictactoe/migrations/0001_initial.py
+++ demo/boardgames/tictactoe/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.core.validators
 from django.conf import settings
 
 
@@ -18,19 +19,35 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
                 ('start_time', models.DateTimeField(auto_now_add=True)),
                 ('last_active', models.DateTimeField(auto_now=True)),
+                ('status', models.CharField(max_length=1, default='A', choices=[('A', 'Active'), ('F', 'First Player Wins'), ('S', 'Second Player Wins'), ('D', 'Draw')])),
                 ('first_player', models.ForeignKey(related_name='games_first_player', to=settings.AUTH_USER_MODEL)),
                 ('next_to_move', models.ForeignKey(related_name='games_to_move', to=settings.AUTH_USER_MODEL)),
                 ('second_player', models.ForeignKey(related_name='games_second_player', to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
+            name='Invitation',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('message', models.CharField(verbose_name='Optional Message', max_length=300, blank=True, help_text='Adding a friendly message is never a bad idea!')),
+                ('timestamp', models.DateTimeField(auto_now_add=True)),
+                ('from_user', models.ForeignKey(related_name='invitations_sent', to=settings.AUTH_USER_MODEL)),
+                ('to_user', models.ForeignKey(verbose_name='User to invite', help_text='Please select the user you want to play a game with', related_name='invitations_received', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
             name='Move',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
-                ('x', models.IntegerField()),
-                ('y', models.IntegerField()),
+                ('x', models.IntegerField(validators=[django.core.validators.MinValueValidator(0), django.core.validators.MaxValueValidator(2)])),
+                ('y', models.IntegerField(validators=[django.core.validators.MinValueValidator(0), django.core.validators.MaxValueValidator(2)])),
                 ('comment', models.CharField(max_length=300)),
+                ('by_first_player', models.BooleanField()),
+                ('timestamp', models.DateTimeField(auto_now_add=True)),
                 ('game', models.ForeignKey(to='tictactoe.Game')),
             ],
+            options={
+                'get_latest_by': 'timestamp',
+            },
         ),
     ]
