3ba39ba1ec19104dc3fb392b327cd3eb227cd4fc
nashfleet <nashf170@gmail.com>
2017-04-11 19:28:40 -0500

    Late additions

--- demo/boardgames/tictactoe/views.py
+++ demo/boardgames/tictactoe/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
-
+from django.views.generic import ListView
 from .models import Invitation, Game, Move
 from .forms import InvitationForm, MoveForm
+
+class AllGamesList(ListView):
+    model = Game
 
 @login_required
 def new_invitation(request):
--- demo/boardgames/userprofile/views.py
+++ demo/boardgames/userprofile/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.core.urlresolvers import reverse_lazy
 
 from tictactoe.models import Game
 
@@ -16,3 +19,8 @@ def home(request):
              'finished_games': finished_games,
              'invitations': invitations}
   return render(request, "userprofile/home.html", context)
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = "user/signup.html"
+    success_url = reverse_lazy('userprofile_home')
